76e027d0a32bbd3c2251a197c0fa7ad6d05111f5
Matthew Muccio <me@matthewmuccio.com>
2018-06-10 22:37:44 -0400

    Adds more test cases to ensure methods are working correctly.

--- tests.py
+++ tests.py
@@ -6,11 +6,19 @@ from bankmodelproject import Account, Bank, Customer, Employee, Person
 
 if __name__ == "__main__":
 	account = Account("123456789", "checking", 100, "US", "1234")
-	bank = Bank("Matt's Bank", 0, [], [], [])
+	bank = Bank("Matt's Bank", 100, [], [], [])
 	person = Person("Matthew", "Muccio", "male", 20, "matthewmuccio", "password")
 	person2 = Person("Patrick", "Star", "male", 18, "pstar123", "123123")
 	customer = Customer(person, account)
 	employee = Employee(person, "Owner", 1000)
+
+	account.deposit(500)
+	account.withdraw(100)
+
+	bank.deposit(500)
+	bank.withdraw(100)
+
+	employee.job = "CEO"
 
 	print("Account object:")
 	print(account)
